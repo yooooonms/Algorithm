96017132dc9c096ce9cafcaa4c61ba64bd0195bb
yooooonms <yooooonms@gmail.com>
2021-07-01 16:34:05 +0900

    [baekjoon] 1로 만들기

---
+++ baekjoon/1463.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(br.readLine());
+        int[] dp = new int[x + 1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= x; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        bw.write(String.valueOf(dp[x]));
+        bw.flush();
+        bw.close();
+    }
+}
