ce724e9445ab9590a3ed649887329370ce695509
yooooonms <yooooonms@gmail.com>
2021-07-06 21:13:56 +0900

    [baekjoon] 2xn 타일링

---
+++ baekjoon/11726.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] dp = new int[1001];
+
+        int n = Integer.parseInt(br.readLine());
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+    }
+}
