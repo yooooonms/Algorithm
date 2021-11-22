af18add269a96fece1284cdbe7f74f7588d9aa4a
Yoon Min Soo <86667006+yooooonms@users.noreply.github.com>
2021-11-22 23:20:47 +0900

    [baekjoon] 빠른 A+B

---
+++ baekjoon/implementation/15552.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            int result = a + b;
+            bw.write(String.valueOf(result));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
